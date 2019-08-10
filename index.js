dae05f629044bb0e665e3e997c519a4c2d10feb4
Rasmus Porsager <rasmus@porsager.com>
2019-08-10 15:09:43 +0200

    Only match named param if not empty

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function prepareString(match, use) {
   }
 
   const names = named.map(n => n.slice(2))
-  const regex = new RegExp('^' + match.replace(/:.+?(\/|$)/g, '([^/]*?)/') + '?$')
+  const regex = new RegExp('^' + match.replace(/:.+?(\/|$)/g, '([^/]+?)/') + '?$')
   return function(req, res) {
     const result = req.pathname.match(regex)
     if (result) {
